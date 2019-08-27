b3e8660c20eb63aea3181af62d226490c59fd29a
Rob Madole <robmadole@gmail.com>
2019-08-27 12:53:39 -0500

    Capitalize key env var

--- hex-publish/entrypoint.sh
+++ hex-publish/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mix hex.organization auth fortawesome --key $hex_organization_key
+mix hex.organization auth fortawesome --key $HEX_ORGANIZATION_KEY
 mix hex.publish package --organization fortawesome
